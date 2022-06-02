7901ef667d82dd74e607f91259848f5efd06b53a
TimHsu@M1PRO-20220208 <maochindada@gmail.com>
2022-06-02 12:47:24 +0800

    docs(script): modify makefile style, add pre commit, remove callvis install

--- scripts/gomod_update.sh
+++ scripts/gomod_update.sh
@@ -6,7 +6,6 @@ rm go.sum
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install github.com/swaggo/swag/cmd/swag@latest
-go install github.com/ofabry/go-callvis@latest
 
 go mod init toc-machine-trading
 go mod tidy
