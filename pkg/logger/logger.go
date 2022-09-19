36c55cf986ab77196db4635fbd6dab0a0532975c
TimHsu@M1PRO-20220208 <maochindada@gmail.com>
2022-09-19 13:39:29 +0800

    feat(logger): add logger struct, modify file log format

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -15,23 +15,22 @@ import (
 )
 
 var (
-	globalLogger *logrus.Logger
+	globalLogger *Logger
 	once         sync.Once
 )
 
+// Logger -.
+type Logger struct {
+	*logrus.Logger
+}
+
 // Get Get
-func Get() *logrus.Logger {
+func Get() *Logger {
 	if globalLogger != nil {
 		return globalLogger
 	}
 
 	once.Do(initLogger)
-
-	for {
-		if globalLogger != nil {
-			break
-		}
-	}
 	return globalLogger
 }
 
@@ -52,62 +51,61 @@ func initLogger() {
 	logCfg := parseEnvToLogConfig()
 	newLogger := logrus.New()
 
+	jsonFormatter := &logrus.JSONFormatter{
+		DisableHTMLEscape: true,
+		TimestampFormat:   time.RFC3339,
+		PrettyPrint:       false,
+		CallerPrettyfier:  newCallerPrettyfier(basePath, true),
+	}
+
+	textFormatter := &logrus.TextFormatter{
+		TimestampFormat:  time.RFC3339,
+		FullTimestamp:    true,
+		QuoteEmptyFields: true,
+		PadLevelText:     false,
+		ForceColors:      true,
+		CallerPrettyfier: newCallerPrettyfier(basePath, false),
+	}
+
 	var formatter logrus.Formatter
 	if logCfg.jsonFormat {
-		formatter = &logrus.JSONFormatter{
-			DisableHTMLEscape: true,
-			TimestampFormat:   time.RFC3339,
-			PrettyPrint:       false,
-		}
+		formatter = jsonFormatter
 	} else {
-		formatter = &logrus.TextFormatter{
-			TimestampFormat:  "2006/01/02 15:04:05",
-			FullTimestamp:    true,
-			QuoteEmptyFields: true,
-			PadLevelText:     false,
-			ForceColors:      true,
-			ForceQuote:       true,
-			CallerPrettyfier: newCallerPrettyfier(basePath),
-		}
+		formatter = textFormatter
 	}
 
 	if logCfg.isDev {
 		newLogger.SetReportCaller(true)
 	}
 
+	newLogger.Hooks.Add(fileHook(basePath, jsonFormatter))
 	newLogger.SetFormatter(formatter)
 	newLogger.SetLevel(logrus.Level(logCfg.logLevel))
 	newLogger.SetOutput(os.Stdout)
-	newLogger.Hooks.Add(fileHook(basePath))
 
-	globalLogger = newLogger
+	globalLogger = &Logger{newLogger}
 }
 
-func fileHook(basePath string) *lfshook.LfsHook {
-	date := time.Now().Format(time.RFC3339)
-	date = strings.ReplaceAll(date, ":", "")
-
-	pathMap := lfshook.PathMap{
-		logrus.PanicLevel: filepath.Join(basePath, fmt.Sprintf("/logs/tmt-%s.log", date)),
-		logrus.FatalLevel: filepath.Join(basePath, fmt.Sprintf("/logs/tmt-%s.log", date)),
-		logrus.ErrorLevel: filepath.Join(basePath, fmt.Sprintf("/logs/tmt-%s.log", date)),
-		logrus.WarnLevel:  filepath.Join(basePath, fmt.Sprintf("/logs/tmt-%s.log", date)),
-		logrus.InfoLevel:  filepath.Join(basePath, fmt.Sprintf("/logs/tmt-%s.log", date)),
-	}
-
+func fileHook(basePath string, formatter logrus.Formatter) *lfshook.LfsHook {
+	date := strings.ReplaceAll(time.Now().Format(time.RFC3339), ":", "")
 	return lfshook.NewHook(
-		pathMap,
-		&logrus.JSONFormatter{
-			DisableHTMLEscape: true,
-			TimestampFormat:   time.RFC3339,
-			PrettyPrint:       true,
+		lfshook.PathMap{
+			logrus.PanicLevel: filepath.Join(basePath, fmt.Sprintf("/logs/tmt-%s.log", date)),
+			logrus.FatalLevel: filepath.Join(basePath, fmt.Sprintf("/logs/tmt-%s.log", date)),
+			logrus.ErrorLevel: filepath.Join(basePath, fmt.Sprintf("/logs/tmt-%s.log", date)),
+			logrus.WarnLevel:  filepath.Join(basePath, fmt.Sprintf("/logs/tmt-%s.log", date)),
+			logrus.InfoLevel:  filepath.Join(basePath, fmt.Sprintf("/logs/tmt-%s.log", date)),
 		},
+		formatter,
 	)
 }
 
-func newCallerPrettyfier(basePath string) func(*runtime.Frame) (function string, file string) {
+func newCallerPrettyfier(basePath string, isJSON bool) func(*runtime.Frame) (function string, file string) {
 	return func(frame *runtime.Frame) (function string, file string) {
 		fileName := strings.ReplaceAll(frame.File, fmt.Sprintf("%s/", basePath), "")
+		if isJSON {
+			return fmt.Sprintf("%s:%d", fileName, frame.Line), ""
+		}
 		return fmt.Sprintf("[%s:%d]", fileName, frame.Line), ""
 	}
 }
