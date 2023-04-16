e43e3c6a320c3bde2627807bb553e6f5e70f81a6
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2023-04-16 22:31:48 +0800

    refactor: add foreign key constraints and indexes for inventory tables
    - Add index on column "stock_num" for table "inventory_stock"
    - Add foreign key constraint "fk_inventory_stock_future" for column "stock_num" on table "inventory_stock" referencing "number" column on table "basic_stock"
    - Add index on column "code" for table "inventory_future"
    - Add foreign key constraint "fk_inventory_future_future" for column "code" on table "inventory_future" referencing "code" column on table "basic_future"
    
    Signed-off-by: Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>

--- migrations/2023041321_migration.up.sql
+++ migrations/2023041321_migration.up.sql
@@ -7,6 +7,9 @@ CREATE TABLE inventory_stock (
     "quantity" INT NOT NULL,
     "stock_num" VARCHAR NOT NULL
 );
+CREATE INDEX inventory_stock_stock_num_index ON inventory_stock USING btree ("stock_num");
+ALTER TABLE inventory_stock
+ADD CONSTRAINT "fk_inventory_stock_future" FOREIGN KEY ("stock_num") REFERENCES basic_stock ("number");
 CREATE TABLE inventory_future (
     "id" SERIAL PRIMARY KEY,
     "updated" TIMESTAMPTZ NOT NULL,
@@ -15,4 +18,7 @@ CREATE TABLE inventory_future (
     "quantity" INT NOT NULL,
     "code" VARCHAR NOT NULL
 );
+CREATE INDEX inventory_future_code_index ON inventory_future USING btree ("code");
+ALTER TABLE inventory_future
+ADD CONSTRAINT "fk_inventory_future_future" FOREIGN KEY ("code") REFERENCES basic_future ("code");
 COMMIT;
