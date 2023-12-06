34604d679c29190bbcdb746b0f6b906b80e2fb9d
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2023-12-06 21:40:28 +0800

    refactor: standardize documentation and command outputs
    - Remove the timestamp from the generated docs package comment
    - Modify the `swag init` command to not include the generated time

--- scripts/generate_swagger.sh
+++ scripts/generate_swagger.sh
@@ -3,7 +3,7 @@
 echo "Generating swagger docs..."
 echo 'package main' >./tradebot.go
 swag fmt
-swag init --generatedTime=true -q -g internal/controller/http/router/router.go
+swag init -q -g internal/controller/http/router/router.go
 rm -rf ./tradebot.go
 echo "" >>./docs/swagger.json
 git add ./docs
