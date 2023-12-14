bc8c48dd127e0e68ed914c3cddc9743b61d01889
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2023-12-14 13:40:02 +0800

    refactor: refactor API and middleware for Swagger integration
    - Updated the API version from `0.0.1` to `1.0.0`
    - Removed the usage of `corsMiddleware()` from the `NewRouter()` function
    - Added a conditional check for the `DISABLE_SWAGGER_HTTP_HANDLER` environment variable
    - Implemented the `swaggerMiddleware()` when the `DISABLE_SWAGGER_HTTP_HANDLER` environment variable is not set
    - Removed CORS configuration from `corsMiddleware()`, which is now `swaggerMiddleware()`, focusing solely on setting the Swagger host information

--- internal/controller/http/router/router.go
+++ internal/controller/http/router/router.go
@@ -30,7 +30,7 @@ type Router struct {
 //
 //	@title			TOC MACHINE TRADING
 //	@description	API docs for Auto Trade
-//	@version		0.0.1
+//	@version		1.0.0
 //	@license.name	Apache 2.0
 //	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
 func NewRouter() *Router {
@@ -39,12 +39,14 @@ func NewRouter() *Router {
 
 	handler := gin.New()
 	handler.Use(gin.Recovery())
-	handler.Use(corsMiddleware())
 
 	// Swagger
 	if os.Getenv("DISABLE_SWAGGER_HTTP_HANDLER") != "" {
 		handler.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	} else {
+		handler.Use(swaggerMiddleware())
 	}
+
 	handler.GET("/metrics", gin.WrapH(promhttp.Handler()))
 	handler.GET(fmt.Sprintf("%s/-/health", prefixV1), healthCheck)
 
@@ -108,17 +110,9 @@ func healthCheck(c *gin.Context) {
 	c.JSON(http.StatusOK, "OK")
 }
 
-func corsMiddleware() gin.HandlerFunc {
+func swaggerMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		docs.SwaggerInfo.Host = c.Request.Host
-		method := c.Request.Method
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "*")
-		c.Header("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE")
-		c.Set("content-type", "application/json")
-		if method == "OPTIONS" {
-			c.JSON(http.StatusOK, nil)
-		}
 		c.Next()
 	}
 }
