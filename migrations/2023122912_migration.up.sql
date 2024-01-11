b171e2ec131a46f28e199512c2f6fa7bc3d88793
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2024-01-11 15:49:09 +0800

    refactor: "Update system_account schema and test tolerances"
    - Change `push_token` column in `system_account` table to accept null values.

--- migrations/2023122912_migration.up.sql
+++ migrations/2023122912_migration.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE system_account (
     "username" VARCHAR NOT NULL,
     "password" VARCHAR NOT NULL,
     "email" VARCHAR NOT NULL,
-    "push_token" VARCHAR NOT NULL,
+    "push_token" VARCHAR,
     "email_verified" BOOLEAN NOT NULL DEFAULT FALSE,
     "auth_trade" BOOLEAN NOT NULL DEFAULT FALSE
 );
