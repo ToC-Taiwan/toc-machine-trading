9af20d63c119d698be98ab54302def37879f804a
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2024-01-16 22:07:19 +0800

    refactor: refactor error handling in user registration process
    - Remove the `ErrEmailNotMatch` error from the list of use case errors
    - Adjust error codes for `ErrEmailAlreadyExists` and `ErrUsernameAlreadyExists`

--- internal/usecase/errors.go
+++ internal/usecase/errors.go
@@ -13,7 +13,6 @@ var (
 	ErrUserNotFound          = &UseCaseError{Code: -1001, Message: "user not found"}
 	ErrPasswordNotMatch      = &UseCaseError{Code: -1002, Message: "password not match"}
 	ErrEmailNotVerified      = &UseCaseError{Code: -1003, Message: "email not verified"}
-	ErrEmailNotMatch         = &UseCaseError{Code: -1004, Message: "email not match"}
-	ErrEmailAlreadyExists    = &UseCaseError{Code: -1005, Message: "email already exists"}
-	ErrUsernameAlreadyExists = &UseCaseError{Code: -1006, Message: "username already exists"}
+	ErrEmailAlreadyExists    = &UseCaseError{Code: -1004, Message: "email already exists"}
+	ErrUsernameAlreadyExists = &UseCaseError{Code: -1005, Message: "username already exists"}
 )
